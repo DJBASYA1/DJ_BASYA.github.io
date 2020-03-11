7c630648c5cd0c8c58400f3fba5e7d2abaabaa79
DJBASYA1 <41920195+DJBASYA1@users.noreply.github.com>
2020-03-11 14:32:53 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('menu-btn_active');
-});
-
-new WOW().init();
-
-
-
-var target = $('.skillbar-wrap');
-var targetPos = target.offset().top;
-var winHeight = $(window).height();
-var scrollToElem = targetPos - winHeight;
-$(window).scroll(function() {
-  var winScrollTop = $(this).scrollTop();
-  if(winScrollTop > scrollToElem) {
-    $('.skillbar').each(function() {
-      $(this).find('.skillbar-bar').animate({
-        width:$(this).attr('data-percent')
-      }, 4000)
-    })
-  }
+$(document).ready(function() {
+  $('a[href^="#"').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({
+       scrollTop:$(target).offset().top
+    }, 1000);
+    return false;
+  });
+  
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0)
+       $('#toTop').fadeIn();
+    else
+       $('#toTop').fadeOut();
+  });
+  $('#toTop').click(function() {
+    $('body, html').animate({
+       scrollTop: 0
+    }, 1000);
+  });
+  
+  $('.menu-btn').on('click', function(e) {
+      e.preventDefault;
+      $(this).toggleClass('menu-btn_active');
+  });
+  
+  var target = $('.skillbar-wrap');
+  var targetPos = target.offset().top;
+  var winHeight = $(window).height();
+  var scrollToElem = targetPos - winHeight;
+  $(window).scroll(function() {
+    var winScrollTop = $(this).scrollTop();
+    if(winScrollTop > scrollToElem) {
+      $('.skillbar').each(function() {
+        $(this).find('.skillbar-bar').animate({
+          width:$(this).attr('data-percent')
+        }, 4000)
+      })
+    }
+  });
+  
+  new WOW().init();
 });
